2995ba988a68ac8ae6f436855aa33108f583e2ec
Zach <45150570+zweatshirt@users.noreply.github.com>
2024-12-20 23:31:21 -0600

    more hashmap stuff

--- src/hashmaps.rs
+++ src/hashmaps.rs
@@ -4,6 +4,8 @@ use std::collections::HashMap;
 * Stores key value pairs
 * Classic map
 * HashMap<K, V>
+* O(1) addition + deletion but of course
+* buckets can overlap reducing performance
 */
 
 pub fn make_maps() {
@@ -32,4 +34,111 @@ pub fn make_maps() {
     let to_remove = &to_find;
     population.remove(to_remove);
     println!("Tokyo removed, new map: {:?}", population);
+
+    // iterating over hashmap
+    for (city, pop) in &population {
+        println!("{}'s population is {}", city, pop);
+    }
+
+    for (city, pop) in &mut population {
+        if *pop < 20_000_000 {
+            let orig_pop = *pop;
+            *pop = 20_000_000;
+            println!("Updated {}'s population from {} to {}", city, orig_pop, pop);
+        }
+    }
+
+    // iterate over values
+    for val in population.values() {
+        println!("Population: {}", val)
+    }
+
+    // Inserts Delhi if it doesn't exist else inserts 0 as the value
+    // Delhi is not the map so add it
+    population.entry("Delhi").or_insert(0);
+
+    // Tokyo exists, so insert 0 as the value
+    population.entry("Tokyo").or_insert(0);
+    println!("{:?}", population);
 }
+
+pub fn map_with_cap() {
+    let mut scores = HashMap::with_capacity(10);
+    for i in 0..10 {
+        scores.insert(i, i*10);
+    }
+    println!("{:?}, capacity: {}", scores, scores.capacity());
+
+    // capacity shows 14 in output instead of 10, why?
+    // actual capacity may vary due to internal HashMap algorithm
+    // also hash map is unordered data struct
+}
+
+/* Student grades tracker challenge */
+pub fn map_challenge() {
+    /*
+    * key: String
+    * value: i32
+    * 1. program should add grades
+    * for multiple students
+    * 2. Find specific student's grade
+    * 3. be able to update student's grade
+    * 4. Remove student
+    * 5. Print all students and their grade
+     */
+
+    let mut map = HashMap::new();
+    map.insert(String::from("Joe"), 90);
+    map.insert(String::from("Jim"), 80);
+    map.insert(String::from("Alphaeus"), 80);
+
+    // 1
+    let student = String::from("Joe");
+    let grade = 70;
+    update_grade(&student, &grade, &mut map);
+    
+    // 2
+    let student = String::from("Jim");
+    let grade = find_grade_for(&student, &map);
+    println!("Student {}'s grade is {}", student, grade);
+
+    // 3 same as add_grade in 1
+    
+    // 4 bye Jim
+    remove_student(&student, &mut map);
+
+    // 5
+    print_all_grades(&map);
+}
+
+fn update_grade(student: &String, grade: &i32, map: &mut HashMap<String, i32>) {
+    map.entry(student.to_string()).or_insert(*grade); 
+    println!("Updated map: {:?}", map);
+}
+
+fn find_grade_for(student: &String, map: &HashMap<String, i32>) -> i32 {
+    match map.get(student) {
+        Some(&grade) => { 
+            return grade;
+        }
+        None =>
+        { 
+            println!("Student {} doesn't exist in the map", student);
+            return -1;
+        }
+    }
+}
+
+fn remove_student(student: &String, map: &mut HashMap<String, i32>) {
+    map.remove(student);
+    println!("Bye {}.", student);
+}
+
+fn print_all_grades(map: &HashMap<String, i32>) {
+    for (student, grade) in map {
+        println!("{}'s grade is {}", student, grade);
+    }
+}
+
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use core::hash;
+
 mod vectors;
 mod hashmaps;
 
@@ -5,4 +7,6 @@ fn main() {
     vectors::make_vecs();
     vectors::iter_vec();
     hashmaps::make_maps();
+    hashmaps::map_with_cap();
+    hashmaps::map_challenge();
 }
